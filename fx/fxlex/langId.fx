04d738bb64c60518fcf9f71897d8272eb270370e
Pablo Moreno Vera <pablomvera14@gmail.com>
2019-05-17 13:30:59 +0200

    Add files via upload

---
+++ fx/fxlex/langId.fx
@@ -0,0 +1,3 @@
+type record vector;
+type record difficult;
+dsdjhbhdsldf;
---
+++ fx/fxparser/parser_test.go
@@ -0,0 +1,29 @@
+package fxparser
+
+import (
+	"bufio"
+	"fmt"
+	"fx/fxlex2"
+	"os"
+	"testing"
+)
+
+func TestNewLexer(t *testing.T) {
+	var token fxlex2.Token
+
+	f, err := os.Open("lang.fx")
+	defer f.Close()
+	if err != nil {
+		print(err)
+	}
+
+	r := bufio.NewReader(f)
+	parser := NewParser(fxlex2.NewLexer(r, "lang.fx"))
+	fmt.Println("Parser:", parser)
+
+	for token.Type != fxlex2.TokEOF {
+		token, err = parser.l.Lex()
+		fmt.Println("Lexema:", token.Lexema, fmt.Sprintf("Type:%v", token.Type), "Valor:", token.Valor)
+	}
+
+}
